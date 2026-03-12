b2f1844fed9390a06d8351a67c0407f43340469a
Kim-Gijs-Digits <kim.gijs.1983@gmail.com>
2026-03-12 17:08:20 +0100

    improve offline startup with cache

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,38 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "shift-tap-cache-v1";
+
+const APP_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./pdf-export.js",
+  "./jspdf.umd.min.js",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./ST-logo.png",
+  "./lock_open.svg",
+  "./lock_close.svg",
+  "./support.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    }).catch(() => {
+      return caches.match("./index.html");
+    })
+  );
 });
